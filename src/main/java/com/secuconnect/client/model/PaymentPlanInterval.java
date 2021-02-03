04396d609c5c695b8c8c9864bf1e5fbe6bd0dd40
eugene <trogvar22@gmail.com>
2021-02-03 16:20:37 +0200

    create payment plan and subscription

---
+++ src/main/java/com/secuconnect/client/model/PaymentPlanInterval.java
@@ -0,0 +1,88 @@
+package com.secuconnect.client.model;
+
+import com.google.gson.annotations.SerializedName;
+import java.util.Objects;
+
+/**
+ * PaymentPlanInterval
+ */
+public class PaymentPlanInterval {
+  @SerializedName("unit")
+  private String unit = null;
+
+  @SerializedName("count")
+  private Integer count = null;
+
+  public PaymentPlanInterval unit(String unit) {
+    this.unit = unit;
+    return this;
+  }
+
+   /**
+   * Unit
+   * @return unit
+  **/
+  public String getUnit() {
+    return unit;
+  }
+
+  public void setUnit(String unit) {
+    this.unit = unit;
+  }
+
+  public PaymentPlanInterval count(Integer count) {
+    this.count = count;
+    return this;
+  }
+
+   /**
+   * Count
+   * @return count
+  **/
+  public Integer getCount() {
+    return count;
+  }
+
+  public void setCount(Integer count) {
+    this.count = count;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    PaymentPlanInterval paymentPlanInterval = (PaymentPlanInterval) o;
+    return Objects.equals(this.unit, paymentPlanInterval.unit) &&
+        Objects.equals(this.count, paymentPlanInterval.count);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(unit, count);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class PaymentPlanInterval {\n");
+    sb.append("    unit: ").append(toIndentedString(unit)).append("\n");
+    sb.append("    count: ").append(toIndentedString(count)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
---
+++ src/main/java/com/secuconnect/client/model/PaymentSubscriptionsProductModelBillingCycles.java
@@ -0,0 +1,99 @@
+package com.secuconnect.client.model;
+
+import com.google.gson.annotations.SerializedName;
+import com.secuconnect.client.model.BillingCyclesItem;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Billing cycles
+ */
+public class PaymentSubscriptionsProductModelBillingCycles {
+  @SerializedName("cycle_executions")
+  private List<BillingCyclesItem> cycleExecutions = null;
+
+  @SerializedName("next_billing_time")
+  private String nextBillingTime = null;
+
+  public PaymentSubscriptionsProductModelBillingCycles cycleExecutions(List<BillingCyclesItem> cycleExecutions) {
+    this.cycleExecutions = cycleExecutions;
+    return this;
+  }
+
+  public PaymentSubscriptionsProductModelBillingCycles addCycleExecutionsItem(BillingCyclesItem cycleExecutionsItem) {
+    if (this.cycleExecutions == null) {
+      this.cycleExecutions = new ArrayList<>();
+    }
+    this.cycleExecutions.add(cycleExecutionsItem);
+    return this;
+  }
+
+   /**
+   * cycle executions
+   * @return cycleExecutions
+  **/
+  public List<BillingCyclesItem> getCycleExecutions() {
+    return cycleExecutions;
+  }
+
+  public void setCycleExecutions(List<BillingCyclesItem> cycleExecutions) {
+    this.cycleExecutions = cycleExecutions;
+  }
+
+  public PaymentSubscriptionsProductModelBillingCycles nextBillingTime(String nextBillingTime) {
+    this.nextBillingTime = nextBillingTime;
+    return this;
+  }
+
+   /**
+   * next billing time
+   * @return nextBillingTime
+  **/
+  public String getNextBillingTime() {
+    return nextBillingTime;
+  }
+
+  public void setNextBillingTime(String nextBillingTime) {
+    this.nextBillingTime = nextBillingTime;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    PaymentSubscriptionsProductModelBillingCycles paymentSubscriptionsProductModelBillingCycles = (PaymentSubscriptionsProductModelBillingCycles) o;
+    return Objects.equals(this.cycleExecutions, paymentSubscriptionsProductModelBillingCycles.cycleExecutions) &&
+        Objects.equals(this.nextBillingTime, paymentSubscriptionsProductModelBillingCycles.nextBillingTime);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(cycleExecutions, nextBillingTime);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class PaymentSubscriptionsProductModelBillingCycles {\n");
+    sb.append("    cycleExecutions: ").append(toIndentedString(cycleExecutions)).append("\n");
+    sb.append("    nextBillingTime: ").append(toIndentedString(nextBillingTime)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
